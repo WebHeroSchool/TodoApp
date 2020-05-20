6646fcee087e8b8c65f498da5f882e36d345a736
Maria Kasatkina <56779635+MariaKasa@users.noreply.github.com>
2020-05-20 15:43:34 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+const user = 'By Maria '
+const num = 777
+const count = 1000 + 500;
+const log = false;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1 style = {{color: '#7B68EE'}}>My first app</h1>
+        {user}
+        {num}
+        {count}
+        {log && 'is true'}
+        {log ? 1 : 2}
+        {null}
+        {undefined}
+        {false}
+        {true}
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
