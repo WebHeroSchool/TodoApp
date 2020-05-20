1e9f5537a7b5f38bbddec5b068c37505842ff94f
Maria Kasatkina <56779635+MariaKasa@users.noreply.github.com>
2020-05-20 16:33:52 +0300

    add file number.js
    Для зададания "Модули"

---
+++ number.js
@@ -0,0 +1,15 @@
+
+
+const count = 1;
+const length = 979
+console.log( 'count * length =', count * length)
+export { count , length };
+
+
+// const count = 23;
+// const length = 3;
+// export { count, length };
+
+// const c = 23;
+// const l = 3;
+// export { c as count, l as length };
