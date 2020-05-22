b3553c8c120e27ef31af4d4cac7caaf6bef28586
Maria Kasatkina <56779635+MariaKasa@users.noreply.github.com>
2020-05-22 23:03:28 +0300

    Delete files

--- src/App.js
+++ src/App.js
@@ -1,40 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-const user = 'By Maria '
-const num = 777
-const counts = 1000 + 500;
-const log = false;
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 style = {{color: '#7B68EE'}}>My first app</h1>
-        {user}
-        {num}
-        {count}
-        {log && 'is true'}
-        {log ? 1 : 2}
-        {null}
-        {undefined}
-        {false}
-        {true}
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const element = (<h1 style = {{color: '#191970'}}> Hello </h1>)
 
-export default App;
+  export {element}
