47da47c55984c88a05db3bac07055ea8c353c7b7
Maria Kasatkina <56779635+MariaKasa@users.noreply.github.com>
2020-05-23 17:39:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const element = (<h1 style = {{color: '#191970'}}> Hello </h1>)
+const ItemList = () => (<ul><li>Джаз</li>
+  <li>Блюз </li>
+  <li>Классика</li>
+  <li>Рок</li>
+  <li>Рэп</li>
+  <li>Инди</li>
+  <li>Регги</li>
+  <li>Поп</li>
+  </ul>)
+const App = () => (<div><h1 style = {{color: '#000080'}}> Жанры музыки </h1>
+  <ItemList />
+  </div>)
 
-  export {element}
+
+  export default App
