f8285c9b5d2e3f7a86f4717ea952fb5e4061afc5
Maria Kasatkina <56779635+MariaKasa@users.noreply.github.com>
2020-05-25 21:48:00 +0300

    Добавлены компоненты

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import InputItem from '../InputItem/InputItem'
+import ItemList from '../ItemList/ItemList'
+import Footer from '../Footer/Footer'
+
+const todoItem = 'Сделать задание по React'
+
+const App = () => (
+  <div>
+  <h1> To do list </h1>  <InputItem />
+  <ItemList todoItem={todoItem} />
+   <Footer count = {3}/>
+  </div>)
+
+
+  export default App
