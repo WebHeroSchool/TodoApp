7527776e10eb7556322b19edbba6b6537d8b7afb
Maria Kasatkina <kasmasha07@gmail.com>
2020-05-28 15:19:52 +0300

    addui

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<p> InputItem</p>)
+const InputItem = () => (<div><TextField
+          label="Добавить задание"
+          id="filled-size-small"
+          variant="filled"
+          size="small"
+        />
+        </div>)
 export default InputItem
